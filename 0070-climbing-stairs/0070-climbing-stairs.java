de50eb8076d332bfe29c189fd4ef0c5d2a237165
Vinothini Kulandaivel <vinothinikulandaivel1216@gmail.com>
2025-04-06 12:34:58 +0530

    Time: 0 ms (100%), Space: 40.6 MB (27.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,12 +3,12 @@ class Solution {
         if (n == 1) return 1;
         if (n == 2) return 2;
 
-        int first = 1, second = 2, third = 0;
+        int fir= 1, sec= 2, th= 0;
         for (int i = 3; i <= n; i++) {
-            third = first + second;
-            first = second;
-            second = third;
+            th= fir + sec;
+            fir = sec;
+            sec = th;
         }
-        return second;
+        return sec;
     }
 }
